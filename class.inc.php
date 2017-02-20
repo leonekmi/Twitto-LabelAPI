4a1a34c2dec7c37c6a7d4121c112d1084274907e
leonekmi <leonekmi@hotmail.com>
2017-02-20 21:06:11 +0100

    Update class.inc.php

--- class.inc.php
+++ class.inc.php
@@ -13,7 +13,7 @@ function getInfo($user) {
   https://github.com/leonekmi/Twitto-LabelAPI/wiki/getuserinfo
   GetUserInfo
   */
-  return json_decode(file_get_contents("https://www.twittolabel.tech/api/getuserinfo.php?user=" . $user), true);
+  return json_decode(file_get_contents("https://twittolabel.tech/api/getinfo/" . $user), true);
 }
 
 // Ne rien préciser si vous voulez obtenir la liste entière
@@ -22,6 +22,7 @@ function getUsers($label = '') {
   https://github.com/leonekmi/Twitto-LabelAPI/wiki/getusers
   GetUsers
   */
+  // API SUSPENDUE (au 20/02/17)
   if ($label == '') {
    $users = file_get_contents("https://www.twittolabel.tech/api/getusers.php");
    $result = json_decode($users, true);
@@ -38,7 +39,7 @@ function search($user) {
   https://github.com/leonekmi/Twitto-LabelAPI/wiki/search
   Search
   */
-  return json_decode(file_get_contents("https://www.twittolabel.tech/api/search.php?user=" . $user), true);
+  return json_decode(file_get_contents("https://twittolabel.tech/api/search/" . $user), true);
 }
 
 function postRating($user, $rate) {
@@ -46,7 +47,7 @@ function postRating($user, $rate) {
   https://github.com/leonekmi/Twitto-LabelAPI/wiki/postrating
   PostRating
   */
-  return json_decode(file_get_contents("https://www.twittolabel.tech/api/postrating.php?name=" . $user . "&rate=" . $rate), true);
+  return json_decode(file_get_contents("https://www.twittolabel.tech/api/postrating.php/" . $user . "/" . $rate), true);
 }
 
 }
